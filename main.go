78fd28ef24baa2f953e2a71e85f4a52f2611fd66
DamirGukov <122615875+DamirGukov@users.noreply.github.com>
2023-07-10 20:23:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,39 +8,39 @@ import (
 )
 
 type Task struct {
-	ID   int    `json:"id"`
-	Date string `json:"date"`
-	Name string `json:"name"`
+	ID         int    `json:"id"`
+	NumberTask string `json:"number"`
+	NameTask   string `json:"name"`
+	Quantity   int    `json:"quantity"`
+	Reps       int    `json:"reps"`
 }
 
 var tasks = []Task{
-	{ID: 1, Name: "Task1", Date: "07.11.2010"},
-	{ID: 2, Name: "Task2", Date: "21.08.2005"},
-	{ID: 3, Name: "Task3", Date: "10.06.2002"},
-	{ID: 4, Name: "Task4", Date: "16.01.2023"},
-	{ID: 5, Name: "Task5", Date: "07.11.2010"},
+	{ID: 1, NumberTask: "Task1", NameTask: "Push-ups", Quantity: 25, Reps: 4},
+	{ID: 2, NumberTask: "Task2", NameTask: "Pull-ups", Quantity: 15, Reps: 4},
+	{ID: 3, NumberTask: "Task3", NameTask: "Hanging leg raises", Quantity: 20, Reps: 4},
+	{ID: 4, NumberTask: "Task4", NameTask: "Sit-ups", Quantity: 30, Reps: 4},
 }
 
 func main() {
-
 	r := mux.NewRouter()
 
 	r.HandleFunc("/tasks", GetTasks).Methods(http.MethodGet)
 
 	fmt.Println("Server is starting at port 8080")
 	http.ListenAndServe(":8080", r)
-
 }
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
-	date := r.URL.Query().Get("date")
+	number := r.URL.Query().Get("number")
 
+	var filteredTasks []Task
 	for _, task := range tasks {
-		if task.Date == date {
-			json.NewEncoder(w).Encode(task)
+		if task.NumberTask == number {
+			filteredTasks = append(filteredTasks, task)
 		}
-
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(filteredTasks)
 }
